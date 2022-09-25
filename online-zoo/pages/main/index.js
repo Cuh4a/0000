9f579fe74cea18d4a5a36d29aa51d175c66a8689
senchurou <cuh4a@yandex.ru>
2022-09-25 21:03:06 +0300

    feat: add bloks Pickandfeed & testimonials

--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -1,2 +1,7 @@
-
-
+const scrollContainer = document.querySelector(".testimonials__slider");
+let range = document.querySelector('input');
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+    range.value++;
+});
