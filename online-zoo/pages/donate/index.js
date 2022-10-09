fb2148c295715122b8981c7fa06b3a4ca97a09fd
senchurou <cuh4a@yandex.ru>
2022-10-09 15:24:11 +0300

    feat: finished online-zoo task JS

--- online-zoo/pages/donate/index.js
+++ online-zoo/pages/donate/index.js
@@ -32,3 +32,61 @@ document.addEventListener('click', e => {
 
 burger.addEventListener('click', toggleBurger);
 nav.addEventListener('click', closeBurger);
+
+
+// ======================================INPUT=====================================
+let costs = document.querySelectorAll('.progress-label');
+let circles = document.querySelectorAll('.progress-count')
+const INPUT = document.querySelector('input[type=number]');
+let steps = document.querySelectorAll('.step-wizard-item');
+let activeCost = document.querySelector('.active-item');
+
+INPUT.value = activeCost.textContent.slice(1);
+
+steps.forEach(ul => {
+  ul.addEventListener('click', (event) => {
+    let data = ul.getAttribute('data-index');
+    let target = event.target.closest('.progress-count');
+
+    if (target.classList.contains('progress-count')) {
+      costs.forEach(cost => cost.classList.remove('active-item'));
+      circles.forEach(cost => cost.classList.remove('active-count'));
+
+      let dropCost = document.querySelector(`.progress-label[data-index = "${data}"]`);
+      let dropCircle = document.querySelector(`.progress-count[data-index = "${data}"]`);
+
+      dropCircle.classList.add('active-count');
+      dropCost.classList.add('active-item');
+
+      let activeCost = document.querySelector('.active-item');
+      INPUT.value = activeCost.textContent.slice(1);
+    }
+  })
+})
+
+function limitText(limitField, limitNum) {
+  if (limitField.value.length > limitNum) {
+    limitField.value = limitField.value.substring(0, limitNum);
+  }
+}
+
+INPUT.addEventListener('change', (e) => {
+  let change = INPUT.value;
+  let arrCosts = ['5000', '2000', '1000', '500', '250', '100', '50', '25'];
+
+  if (arrCosts.includes(change)) {
+    costs.forEach(cost => cost.classList.remove('active-item'));
+    circles.forEach(cost => cost.classList.remove('active-count'));
+
+    let dropCost = document.querySelector(`.progress-label[data-cost = "${change}"]`);
+    let dropCircle = document.querySelector(`.progress-count[data-cost = "${change}"]`);
+
+    dropCircle.classList.add('active-count');
+    dropCost.classList.add('active-item');
+
+  } else {
+    costs.forEach(cost => cost.classList.remove('active-item'));
+    circles.forEach(cost => cost.classList.remove('active-count'));
+  }
+
+})
--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -1,5 +1,3 @@
-
-
 // =====================================================Burger==================================================================
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.header__nav');
@@ -37,7 +35,7 @@ burger.addEventListener('click', toggleBurger);
 nav.addEventListener('click', closeBurger);
 
 // ==================================================Carusel=====================================================================
-const url = 'https://run.mocky.io/v3/0ab92b4e-4b8c-4440-8ee9-042f57613851';
+const url = 'https://run.mocky.io/v3/0f5862f0-75c2-41a3-8d6f-8a7c4add35cb';
 const SLIDER = document.querySelector('.pets__wrapper');
 const ITEM_LEFT = document.querySelector("#left-slides");
 const ITEM_RIGHT = document.querySelector("#right-slides");
@@ -90,7 +88,7 @@ function randomPets(slides) {
     const numbers = [];
     let rand = 0;
     while (numbers.length < slides) {
-        rand = Math.floor(Math.random() * 6);
+        rand = Math.floor(Math.random() * 8);
         if (!numbers.includes(rand)) numbers.push(rand);
     }
     return numbers;
@@ -155,14 +153,119 @@ function getIndexCard(item) {
 // ==============================================Slider-Range===========================================================
 let slider = document.querySelector("div.slider__wrap");
 let elem = document.querySelector('input[type="range"]');
+const urlUsers = 'https://run.mocky.io/v3/af2a1c18-6c57-43fd-bc4e-99d742394cf9';
+const SLIDER__CARD = document.querySelector('.slider__wrap');
+let countCard = 11;
+let slideNum = randomCard(countCard);
 elem.addEventListener("input", function () {
-    slider.style.left = -((elem.value * 316) - 1264) + "px";
+    slider.style.left = -((elem.value * 289) - 1152) + "px";
+    if (window.matchMedia("(max-width: 1200px)").matches) {
+        elem.max = '12';
+    } else {
+        elem.max = '11';
+    }
 });
 
 
+function randomCard(slides) {
+    const numbers = [];
+    let rand = 0;
+    while (numbers.length < slides) {
+        rand = Math.floor(Math.random() * 11);
+        if (!numbers.includes(rand)) numbers.push(rand);
+    }
+    return numbers;
+}
 
 
+async function getDataCard(url, where) {
+    const res = await fetch(url);
+    const data = await res.json();
 
+    where.innerHTML = ``;
+    for (let i = 0; i < slideNum.length; i++) {
+        let card = data[slideNum[i]];
+        where.innerHTML += `
+                        <div class="slider__item data-index="${slideNum[i]}" data-name = "${data[slideNum[i]].userName}">
+                            <div class="slider__slide">
+                                <div class="slide__fon">
+                                    <div class="slide__info">
+                                        <div class="slide__info-img">
+                                            <img src="${card.image}" alt="user-icon">
+                                        </div>
+                                        <div class="slide__info-user">
+                                            <div class="info__user-name">
+                                                <span>${card.userName}</span>
+                                            </div>
+                                            <div class="info__user-loc">
+                                                <span>${card.location}</span>
+                                            </div>
+                                        </div>
+                                        <div class="slide__info-dat">
+                                            <span>${card.date}</span>
+                                        </div>
+                                    </div>
+                                    <div class="slide__text">
+                                        <p>${card.text}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`;
+    }
+}
+getDataCard(urlUsers, SLIDER__CARD);
+// ==================================================POPUP===========================================
+
+
+const CLOSE_MODAL = document.querySelector('.close-modal');
+const MODAL = document.querySelector('.modal');
+const MODAL_WINDOW = document.querySelector('.modal__content');
+
+SLIDER__CARD.addEventListener('click', (event) => {
+    let target = event.target.closest('.slider__item');
+
+    if (target.classList.contains('slider__item')) {
+        MODAL.classList.add('modal-open');
+        document.body.classList.add('lock');
+
+        MODAL_WINDOW.classList.add('modal-transition');
+        getDataModal(urlUsers, target.dataset.name);
+
+    }
+})
+
+CLOSE_MODAL.addEventListener('click', closeModal)
+function closeModal() {
+    MODAL.classList.remove('modal-open');
+    document.body.classList.remove('lock');
+}
+
+async function getDataModal(url, name) {
+    const res = await fetch(url);
+    const data = await res.json();
+    const names = data.map(e => e.userName);
+
+    const modalImage = document.querySelector('.content__info-image');
+    const modalTextName = document.querySelector('.text__name');
+    const modalTextLoc = document.querySelector('.text__location');
+    const modalTextData = document.querySelector('.text__date');
+    const modalTextFeed = document.querySelector('.content__feedback');
+
+    const cardIndex = names.findIndex(e => e === name);
+
+    modalImage.src = data[cardIndex].image;
+    modalTextName.textContent = data[cardIndex].userName;
+    modalTextLoc.textContent = data[cardIndex].location;
+    modalTextData.textContent = data[cardIndex].date;
+    modalTextFeed.textContent = data[cardIndex].text;
+}
+
+window.onclick = function (event) {
+    if (event.target == MODAL) {
+        MODAL.classList.remove('modal-open');
+        document.body.classList.remove('lock');
+    }
+}
 
 
 
